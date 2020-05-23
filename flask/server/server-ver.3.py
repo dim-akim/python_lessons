1f650ce710a503c5052e054575a86942866e1045
Dmitrii Akimov <akimov@1060.ru>
2020-05-23 16:40:09 +0300

    Добавлен динамический элемент в заголовок h1

--- flask/server/server-ver.3.py
+++ flask/server/server-ver.3.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!<br><a target="_blank" href="index">index</a>'
+    return '''Hello, World!
+    <br>
+    <a target="_blank" href="index">index</a>'''
 
 
 @app.route('/index')
 def index():
     user = {'nickname': 'Арсений'}  # выдуманный пользователь
-    return render_template('index.html')
+    return render_template('index.html', **user)  # nickname='Арсений'
 
 
 if __name__ == '__main__':
