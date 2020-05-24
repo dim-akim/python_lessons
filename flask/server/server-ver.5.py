dcfd4a9d86c2b626ca4b32eea067d7acdcdd067f
dim-akim <akimov@1060.ru>
2020-05-25 00:37:13 +0300

    Добавлен 5 шаг. Использование динамического создания ссылок

---
+++ flask/server/server-ver.5.py
@@ -0,0 +1,30 @@
+"""
+5 шаг. Использование динамического создания ссылок
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return f'''Hello, World!
+    <br>
+    <a target="_blank" href="index">index</a>'''
+
+
+@app.route('/index')
+def index():
+    user = {'nickname': 'Арсений'}  # выдуманный пользователь
+    return render_template('index.html', **user)  # nickname='Арсений'
+
+
+@app.route('/day-<num>')
+def day(num):
+    return render_template(f'day-{num}.html')
+
+
+if __name__ == '__main__':
+    app.run()
