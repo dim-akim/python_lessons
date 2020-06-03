8069365bf0ff2f3f304d23e0aefa9b393725b2ef
dim-akim <akimov@1060.ru>
2020-06-03 22:34:01 +0300

    Добавлен курс по написанию простейшего мессенджера на flask. Основан на интенсиве от Skillbox

---
+++ messenger/server-ver1.py
@@ -0,0 +1,36 @@
+"""
+1 шаг. Запуск простейшего сервера на Flask
+
+Этапы:
+
+Импорт основного класса.
+
+Создание экземпляра класса.
+
+Декоратор, в котором описан путь. По этому URL будет обращаться пользователь.
+
+Функция, которая вызывается при обращении пользователя к пути, указанному в декораторе.
+"""
+
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/status')
+def status():
+    return {
+        'status': 'OK',
+        'name': 'Day-1-Messenger',
+        'time': time.ctime(time.time())
+    }
+
+
+if __name__ == '__main__':
+    app.run()
