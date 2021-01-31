1c327fe5222299f49d69d001ae017fc90f830213
dim-akim <akimov@1060.ru>
2021-01-31 17:42:52 +0300

    Добавил больше пояснений и обработчик с клавиатурой

--- telegram_bot/echo-bot.py
+++ telegram_bot/echo-bot.py
@@ -1,34 +1,29 @@
 from telegram import Bot, Update
 from telegram.ext import MessageHandler, Filters, Updater, CommandHandler
-from telegram.utils.request import Request
 from telegram_bot.settings import TOKEN, ADMIN_IDS
 
 
 def main():
-    # request = Request(
-    #     connect_timeout=1.0,
-    #     read_timeout=0.5,
-    # )
-
-    # Записываем бота в переменную, чтобы можно было к нему обращаться
-    bot = Bot(
-        # request=request,
-        token=TOKEN,
-    )
-    print(bot.get_me())
-
-    # TODO что это?
+    # класс, который ловит обновления из телеграма и передает их диспетчеру
+    # по сути, фронтенд для бота. Лучше создавать, используя параметр token, а не параметр bot
     updater = Updater(
-        bot=bot,
-        use_context=True
+        token=TOKEN,
+        use_context=True  # Для обратной совместимости с версиями 12 и ниже
     )
 
-    # Обработчик события
+    # Обработчик команды
     secret_command_handler = CommandHandler('secret', secret_command)
-    updater.dispatcher.add_handler(secret_command_handler)
 
+    # Обработчик обычного сообщения с фильтром
     message_handler = MessageHandler(Filters.text, do_echo)
-    updater.dispatcher.add_handler(message_handler)
+
+    # Диспетчер распределяет события по обработчикам
+    dispatcher = updater.dispatcher
+
+    # Добавляем обработчики. Порядок важен - если какой-то обработчик поймает событие, то дальше оно не пойдет!
+    dispatcher.add_handler(secret_command_handler)
+
+    dispatcher.add_handler(message_handler)
 
     # Начинаем бесконечную обработку входящих сообщений
     updater.start_polling()
@@ -79,6 +74,40 @@ def secret_command(update: Update, context):
     update.message.reply_text(
         text=reply_text
     )
+
+
+def answer_with_keyboard(update: Update, context: CallbackContext) -> None:
+    # Для клавиатуры нужна матрица (двумерный список) из текстовых элементов
+    # Каждый текстовый элемент - отдельная кнопка
+
+    button_list = [
+        [
+            'ряд1 колонка1',
+            'ряд1 колонка2',
+            'ряд1 колонка3'
+        ],
+        [
+            'ряд2 колонка1',
+            'ряд2 колонка2'
+        ]
+    ]
+    # Можно в качестве кнопок использовать объект KeyboardButton
+    # button_list = [
+    #     [
+    #         KeyboardButton("col1"),
+    #         KeyboardButton("col2")
+    #     ],
+    #     [KeyboardButton("row 2")]
+    # ]
+
+    # Теперь собираем кнопки в клавиатуру - объект ReplyKeyboardMarkup
+    # Для одной кнопки в строке можно использовать одномерный список и метод from_column
+    reply_markup = ReplyKeyboardMarkup(
+        button_list,  # список кнопок
+        resize_keyboard=True,  # сжимает кнопки по вертикали до высоты текста
+        one_time_keyboard=True  # убирает клавиатуру после нажатия на кнопку
+    )
+    update.message.reply_text("Меню с тремя колонками", reply_markup=reply_markup)
 
 
 if __name__ == '__main__':
