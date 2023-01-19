9a004e6800bd7f6b7ed6aab4bfa898cae5fa6cda
Dmitrii Akimov <akimov@1060.ru>
2023-01-19 11:24:00 +0300

    Добавил решения новых задач из ЕГЭ 2023 года

---
+++ ЕГЭ/task_17.py
@@ -0,0 +1,51 @@
+# # numbers = []
+# count = 0
+# max_pare = -20001
+#
+# with open('17_test.txt') as file:
+#     numbers = [int(number) for number in file]
+#     # for number in file:
+#     #     numbers.append(int(number))
+#
+# for i in range(len(numbers) - 1):
+#     if numbers[i] % 3 == 0 or numbers[i+1] % 3 == 0:
+#         count += 1
+#         if numbers[i] + numbers[i+1] > max_pare:
+#             max_pare = numbers[i] + numbers[i+1]
+#         # max_pare = max(max_pare, numbers[i] + numbers[i+1])
+#
+# print(count, max_pare)
+
+with open('17 (1).txt') as file:
+    numbers = [int(number) for number in file]
+
+numbers_even = sorted(i for i in numbers if i % 2 == 0)
+numbers_odd = sorted(i for i in numbers if i % 2 == 1)
+print(len(numbers_even), len(numbers_odd))
+
+count = 0
+found_even = 0
+found_odd = 0
+max_even = 0
+max_odd = 0
+for i in range(len(numbers_even)):
+    if numbers_even[i] % 31 == 0:
+        count += len(numbers_even) - 1 - found_even
+        found_even += 1
+        max_even = max(max_even, numbers_even[i])
+
+for i in range(len(numbers_odd)):
+    if numbers_odd[i] % 31 == 0:
+        count += len(numbers_odd) - 1 - found_odd
+        found_odd += 1
+        max_odd = max(max_odd, numbers_odd[i])
+
+max_sum = max(max_even + numbers_even[-1], max_odd + numbers_odd[-1])
+
+# for i in range(len(numbers) - 1):
+#     for j in range(i+1, len(numbers)):
+#         if (numbers[i] - numbers[j]) % 2 == 0 and (numbers[i] % 31 == 0 or numbers[j] % 31 == 0):
+#             count += 1
+#             max_sum = max(max_sum, numbers[i] + numbers[j])
+
+print(count, max_sum)
--- ЕГЭ/task_8.py
+++ ЕГЭ/task_8.py
@@ -1,22 +1,28 @@
+# count = 0
+# a = 'Е'
+# for b in 'ЕГЭИНФ':
+#     for c in 'ЕГЭИНФ':
+#         for d in 'ЕГЭИНФ':
+#             for e in 'ЕГЭИНФ':
+#                 for f in 'ЭИ':
+#                     s = a + b + c + d + e + f
+#                     if s.count('ФИ') == 2 and s.count('ЕГЭ') == 0:
+#                         count += 1
+# print(count)
+
+start = int('10000', base=8)
+stop = int('77777', base=8)
 count = 0
-a = 'Е'
-for b in 'ЕГЭИНФ':
-    for c in 'ЕГЭИНФ':
-        for d in 'ЕГЭИНФ':
-            for e in 'ЕГЭИНФ':
-                for f in 'ЭИ':
-                    s = a + b + c + d + e + f
-                    if s.count('ФИ') == 2 and s.count('ЕГЭ') == 0:
-                        count += 1
+
+for number in range(start, stop+1):
+    number = oct(number)[2:]
+    if len(number) == len(set(number)):
+        flag = False
+        for i in range(len(number)-1):
+            if int(number[i]) % 2 == int(number[i+1]) % 2:
+                flag = True
+                break
+        if not flag:
+            count += 1
+
 print(count)
-
-
-
-
-
-
-
-
-
-
-
