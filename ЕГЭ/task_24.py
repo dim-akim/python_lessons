cd98827823a1bef3a6a875bada624bd103c35e28
Dmitrii Akimov <akimov@1060.ru>
2023-01-27 12:19:56 +0300

    Добавил задания по ЕГЭ 2023

---
+++ ЕГЭ/task_24.py
@@ -0,0 +1,13 @@
+with open('24_demo.txt') as file:
+    line = file.readline()
+
+count = 1
+max_count = 0
+for i in range(len(line) - 1):  # range(1, len(line)): if line[i] == line[i-1]
+    if line[i] != line[i + 1]:
+        count += 1
+    else:
+        max_count = max(count, max_count)
+        count = 1
+
+print(max_count)
